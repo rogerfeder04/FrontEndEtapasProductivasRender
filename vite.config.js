9bfbbaeced9fc6eaabce52f9b02df9f928ef0f90
rogerfeder04 <rogerfeder0437@gmail.com>
2024-10-18 14:12:49 -0500

    18/10

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,6 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ['@fortawesome/vue-fontawesome']
-    }
-  },
   plugins: [
     vue({
       template: { transformAssetUrls }
